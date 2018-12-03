4ef81c0a02fefa9029b005c7036442a57865b338
manuelitox <manuel2788@gmail.com>
2018-12-03 18:10:58 +0100

    Create the Routers and update the App component

--- src/App.js
+++ src/App.js
@@ -1,46 +1,17 @@
 import React, { Fragment } from 'react'
-import { Provider } from 'react-redux'
-import { Router, Route, Switch } from 'react-router-dom'
+import { ThemeProvider } from 'styled-components'
 
-import logo from './logo.svg';
-import history from './history'
-import StyledApp from './StyledApp'
-import store from './store'
+import theme from './config/theme'
+import Routers from './Routers'
+import GlobalStyles from './GlobalStyles'
 
 const App = () => (
-  <Provider store={ store }>
-    <Router history={ history }>      
-      <Fragment>
-        <Route
-          path="/"
-          render={ () => <header>Header!</header> } />
-          <main>
-            <Switch>
-              <Route 
-                path="/home"
-                render={ () => (
-                  <StyledApp>
-                    <header className="App-header">
-                      <img src={logo} className="App-logo" alt="logo" />
-                      <p>
-                        Edit <code>src/App.js</code> and save to reload.
-                      </p>
-                      <a
-                        className="App-link"
-                        href="https://reactjs.org"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                      >
-                        Learn React
-                      </a>
-                    </header>
-                  </StyledApp>
-                )} />
-            </Switch>
-          </main>
-      </Fragment>
-    </Router>
-  </Provider>
+  <ThemeProvider theme={ theme }>
+    <Fragment>
+      <Routers />
+      <GlobalStyles /> 
+    </Fragment>
+  </ThemeProvider>
 )
 
 export default App;
