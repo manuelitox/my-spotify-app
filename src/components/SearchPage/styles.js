3ab81549e0e791686a6416df5464d0f7395d3fcd
manuelitox <manuel2788@gmail.com>
2018-12-05 13:22:17 +0100

    Improve search input design into search page

--- src/components/SearchPage/styles.js
+++ src/components/SearchPage/styles.js
@@ -6,8 +6,12 @@ const StyledSearchPage = styled.div`
   background-position: center;
   color: white;
   height: 70vh;
+  text-align: center;
   padding: 180px 0;
   width: 100%;
+  @media only screen and (max-width: ${ props => props.theme.size.small }) {
+    padding: 80px 0;
+  }
 `
 
 export default StyledSearchPage
