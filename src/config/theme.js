0430f3afc6fd04fce7a8bd5ef20a0ed4509468e0
manuelitox <manuel2788@gmail.com>
2018-12-06 00:03:57 +0100

    Add new colors into the theme object

--- src/config/theme.js
+++ src/config/theme.js
@@ -4,7 +4,9 @@ const theme = {
   blueDarkText: '#A8A9C1',
   blueLight: '#3D4EC9',
   blueLightText: '#97A4FF',
+  blueLightBorder: '#414C6F',
   textFooter: '#385758',
+  searchSpan: '#F7F7F7',
   offWhite: '#999999',
   shadow: 'rgba(0, 0, 0, .7)',
   size: {
