ddc131c387561a08f4da0d73b65101640e0660d4
manuelitox <manuel2788@gmail.com>
2018-12-06 12:32:20 +0100

    Add position relative into the StylesSearchForm component

--- src/components/generic/SearchForm/styles.js
+++ src/components/generic/SearchForm/styles.js
@@ -2,10 +2,11 @@ import styled from 'styled-components'
 
 const StyledSearchForm = styled.div`
   margin: 20px auto 0;
+  position: relative;
   width: 560px;
   @media only screen and (max-width: ${ props => props.theme.size.small }) {
-    width: 100%;
     padding: 0 20px;
+    width: 100%;
   }
 `
 
