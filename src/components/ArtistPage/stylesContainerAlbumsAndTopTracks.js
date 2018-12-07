f967aa91f8761b975afc6efc0741cf1d51391ff2
manuelitox <manuel2788@gmail.com>
2018-12-07 17:31:20 +0100

    Update styles of the StyledContainerAlbumsAndTopTracks component

--- src/components/ArtistPage/stylesContainerAlbumsAndTopTracks.js
+++ src/components/ArtistPage/stylesContainerAlbumsAndTopTracks.js
@@ -5,6 +5,14 @@ import WithContainer from 'components/generic/Container/'
 const StyledContainerAlbumsAndTopTracks = styled.div`
   display: grid;
   grid-template-columns: 2fr 1fr;
+  padding-bottom: 40px;
+  padding-top: 40px;
+  @media only screen and (max-width: ${ props => props.theme.size.medium }) {
+    grid-template-columns: 1fr;
+  }
+  @media only screen and (max-width: ${ props => props.theme.size.small }) {
+    padding-top: 0;
+  }  
 `
 
 export default WithContainer(StyledContainerAlbumsAndTopTracks)
