6f5bef5e8865a5091bea064aa8c7482d957e9294
manuelitox <manuel2788@gmail.com>
2018-12-10 17:29:27 +0100

    Add color prop into Title component

--- src/components/ArtistPage/Title/index.js
+++ src/components/ArtistPage/Title/index.js
@@ -3,9 +3,10 @@ import PropTypes from 'prop-types'
 
 import StyledTitle from './styles'
 
-const Title = ({ children }) => <StyledTitle>{ children }</StyledTitle>
+const Title = ({ color, children }) => <StyledTitle color={ color }>{ children }</StyledTitle>
 
 Title.propTypes = {
+  color: PropTypes.string,
   children: PropTypes.string.isRequired
 }
 
