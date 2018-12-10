c6d51c2040e5d2f371d79072cc0e1286ac30a3e5
manuelitox <manuel2788@gmail.com>
2018-12-10 19:14:08 +0100

    Integrates NameWithOutSpaces into App

--- src/components/ArtistPage/RelatedArtists/Artist/index.js
+++ src/components/ArtistPage/RelatedArtists/Artist/index.js
@@ -4,6 +4,7 @@ import PropTypes from 'prop-types'
 
 import StyledArtist from './styles'
 import PlaceholderArtist from 'svgs/placeholderArtist.svg'
+import NameWithOutSpaces from 'lib/name-without-spaces'
 
 const Artist = ({ id, name, photo }) => (
   <StyledArtist>
@@ -13,7 +14,7 @@ const Artist = ({ id, name, photo }) => (
       width="40"
       height="40" />
     <h4>{ name }</h4>
-    <Link to={ `/artist/${ name.replace(' ', '-') }/${ id }` } />
+    <Link to={ `/artist/${ NameWithOutSpaces(name) }/${ id }` } />
   </StyledArtist>
 )
 
