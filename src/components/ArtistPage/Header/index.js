cd6910d587f255ed88844fc1a37b171d5e0146c4
manuelitox <manuel2788@gmail.com>
2018-12-10 19:18:18 +0100

    Header's headset icon redirect to initial page

--- src/components/ArtistPage/Header/index.js
+++ src/components/ArtistPage/Header/index.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 import StyledHeader from './styles'
 import SearchFormContainer from 'components/generic/SearchForm/container'
@@ -6,7 +7,9 @@ import { ReactComponent as HeadsetIcon } from 'svgs/headset.svg'
 
 const Header = () => (
   <StyledHeader>
-    <HeadsetIcon height="45" width="55" viewBox="0 0 70 55"/>
+    <Link to="/">
+      <HeadsetIcon height="45" width="55" viewBox="0 0 70 55"/>
+    </Link>
     <SearchFormContainer placeholder="search another artist..." />
   </StyledHeader>
 )
