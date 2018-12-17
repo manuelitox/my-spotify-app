a38f4f9b84de0691af0945d2883827966ec49c99
manuelitox <manuel2788@gmail.com>
2018-12-17 17:18:59 +0100

    Basic PopUp integrations into Albums, Related Artists and Tracks

--- src/components/ArtistPage/Albums/index.js
+++ src/components/ArtistPage/Albums/index.js
@@ -1,23 +1,38 @@
-import React from 'react'
+import React, { Component } from 'react'
 
 import Title from 'components/ArtistPage/Title/'
 import Album from './Album/'
+import PopUp from 'components/generic/PopUp/container'
 import Button from 'components/generic/Button/'
 import StyledWrapperAlbums from './styles'
 
-const Albums = () => (
-  <div>
-    <Title>Albums</Title>
-    <StyledWrapperAlbums>
-      <Album title="Hallelujah" numberTracks={ 12 } releaseDate="2016-06-17" />
-      <Album title="Hallelujah" numberTracks={ 12 } releaseDate="2016-06-17" />
-      <Album title="Hallelujah" numberTracks={ 12 } releaseDate="2016-06-17" />
-      <Album title="Hallelujah" numberTracks={ 12 } releaseDate="2016-06-17" />
-      <Album title="Hallelujah" numberTracks={ 12 } releaseDate="2016-06-17" />
-      <Album title="Hallelujah" numberTracks={ 12 } releaseDate="2016-06-17" />
-    </StyledWrapperAlbums>
-    <Button onClick={ () => console.log('open albums list popup') }>view more albums</Button>
-  </div>
-)
+class Albums extends Component {
+  state = { isOpen: false }
+  
+  togglePopUp = () => this.setState({ isOpen: !this.state.isOpen })
+
+  render () {
+    return (
+      <div>
+        <Title>Albums</Title>
+        <StyledWrapperAlbums>
+          <Album title="Hallelujah" numberTracks={ 12 } releaseDate="2016-06-17" />
+          <Album title="Hallelujah" numberTracks={ 12 } releaseDate="2016-06-17" />
+          <Album title="Hallelujah" numberTracks={ 12 } releaseDate="2016-06-17" />
+          <Album title="Hallelujah" numberTracks={ 12 } releaseDate="2016-06-17" />
+          <Album title="Hallelujah" numberTracks={ 12 } releaseDate="2016-06-17" />
+          <Album title="Hallelujah" numberTracks={ 12 } releaseDate="2016-06-17" />
+        </StyledWrapperAlbums>
+        <Button onClick={ this.togglePopUp }>view more albums</Button>
+        <PopUp isOpen={ this.state.isOpen } togglePopUp={ this.togglePopUp }>
+          <div>
+            <h1>Albums list</h1>
+            <p>description...</p>
+          </div>
+        </PopUp>
+      </div>      
+    )
+  } 
+}
 
 export default Albums
