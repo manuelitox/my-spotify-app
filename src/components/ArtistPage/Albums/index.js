2b44eb7081300d0aeabdc0fdc1368bb29e5f1816
manuelitox <manuel2788@gmail.com>
2019-01-04 18:57:45 +0100

    Integrates PopUpAlbums into the Artists page

--- src/components/ArtistPage/Albums/index.js
+++ src/components/ArtistPage/Albums/index.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react'
 
 import Title from 'components/ArtistPage/Title/'
 import Album from './Album/'
-import PopUp from 'components/generic/PopUp/container'
+import PopUpAlbums from './PopUp/'
 import Button from 'components/generic/Button/'
 import StyledWrapperAlbums from './styles'
 
@@ -24,12 +24,7 @@ class Albums extends Component {
           <Album title="Hallelujah" numberTracks={ 12 } releaseDate="2016-06-17" />
         </StyledWrapperAlbums>
         <Button onClick={ this.togglePopUp }>view more albums</Button>
-        <PopUp isOpen={ this.state.isOpen } togglePopUp={ this.togglePopUp }>
-          <div>
-            <h1>Albums list</h1>
-            <p>description...</p>
-          </div>
-        </PopUp>
+        <PopUpAlbums isOpen={ this.state.isOpen } togglePopUp={ this.togglePopUp } />
       </div>      
     )
   } 
