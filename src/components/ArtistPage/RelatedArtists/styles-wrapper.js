51b1e7111ca004448b3ea158855d9426f6e09b44
manuelitox <manuel2788@gmail.com>
2019-01-04 19:21:54 +0100

    Integrates styles for inPopUp option in the StyledWrapperRelatedArtists component

--- src/components/ArtistPage/RelatedArtists/styles-wrapper.js
+++ src/components/ArtistPage/RelatedArtists/styles-wrapper.js
@@ -1,8 +1,9 @@
 import styled from 'styled-components'
+import PropTypes from 'prop-types'
 
 const StyledWrapperRelatedArtists = styled.div`
   display: grid;
-  grid-template-columns: 1fr 1fr 1fr;
+  grid-template-columns: ${ props => props.inPopup ? '1fr' : '1fr 1fr 1fr' };
   grid-gap: 30px;
   margin: 0 0 20px;
   @media only screen and (max-width: ${ props => props.theme.size.medium }) {
@@ -12,5 +13,9 @@ const StyledWrapperRelatedArtists = styled.div`
     grid-template-columns: 1fr;
   }
 `
+
+StyledWrapperRelatedArtists.propTypes = {
+  inPopup: PropTypes.bool
+}
 
 export default StyledWrapperRelatedArtists
